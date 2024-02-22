de04959ffe2f1eb37b672528785c8925c9ec9510
Peter Evers <pevers90@gmail.com>
2024-02-22 09:09:55 +0100

    chore: make pro_directory_current_get public

--- crates/creo-rs/src/pro_util.rs
+++ crates/creo-rs/src/pro_util.rs
@@ -9,7 +9,7 @@ use crate::pro_toolkit_errors::ProErrors;
 /// # Errors
 ///
 /// * `PRO_TK_E_NOT_FOUND` - The function was unable to find current working directory.
-fn pro_directory_current_get() -> Result<String, ProErrors> {
+pub fn pro_directory_current_get() -> Result<String, ProErrors> {
     unsafe {
         let mut dir_path = [0; creo_sys::PRO_PATH_SIZE as usize];
         let status = creo_sys::ProDirectoryCurrentGet(dir_path.as_mut_ptr());
